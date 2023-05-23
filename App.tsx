896e303a1fce2fb90d6f51ae003bedfb356f0da2
colin-nwk <collinsnwoko05@gmail.com>
2023-05-23 16:06:47 +0100

    asyncstorage install

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
-
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { useState } from "react";
 export default function App() {
+  const [text, setText] = useState<string>("");
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+
       <StatusBar style="auto" />
+      <TextInput
+        multiline={true}
+        style={styles.textinput}
+        value={text}
+        onChangeText={setText}
+      />
+      <Button
+        title="Save Note"
+        onPress={() => {
+          alert(text);
+        }}
+      />
     </View>
   );
 }
@@ -16,5 +30,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textinput: {
+    backgroundColor: "#ffb70342",
+    width: "100%",
+    height: 200,
+    fontSize: 16,
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    paddingBottom: 20,
   },
 });
